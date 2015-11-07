fb1f22db2845f083e5a4941ce6a7648a71c72a6e
Yi Huang <yi@skymizer.com>
2015-11-07 15:38:29 +0200

    Detect our iBeacon.

--- BaggageScanner/app/src/main/java/tw/tii/scanner/baggagescanner/MainActivity.java
+++ BaggageScanner/app/src/main/java/tw/tii/scanner/baggagescanner/MainActivity.java
@@ -1,15 +1,53 @@
 package tw.tii.scanner.baggagescanner;
 
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
+import java.util.jar.Manifest;
+
+public class MainActivity extends AppCompatActivity implements BeaconConsumer {
+
+    private class MyRangeNotifier implements RangeNotifier {
+        @Override
+        public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+            Log.d("YI", "# beacons: " + beacons.size());
+            if (beacons.size() > 0) {
+                Beacon beacon = beacons.iterator().next();
+                Log.d("YI", "beacon ID: " + beacon.getId1() + ", beacon Distance: " + beacon.getDistance());
+            }
+        }
+    }
+
+    private BeaconManager m_BeaconM;
+
+    private final String m_BeaconID = "b9407f30-f5f8-466e-aff9-25556b57fe6d";
+    private final Beacon m_Beacon = new Beacon.Builder().setId1(m_BeaconID).build();
+    private final Region m_Region = new Region("BAGGAGE_SCAN_REGION", m_Beacon.getId1(), null, null);
+    private final String[] m_Permissions = {
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.BLUETOOTH,
+            android.Manifest.permission.BLUETOOTH_ADMIN
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +56,31 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        // Request permission for Android API level >= 23.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (int i = 0; i < m_Permissions.length; i++) {
+                if (this.checkSelfPermission(m_Permissions[i]) != PackageManager.PERMISSION_GRANTED) {
+                    this.requestPermissions(m_Permissions, 0);
+                    break;
+                }
+            }
+        }
+
+
+        m_BeaconM = BeaconManager.getInstanceForApplication(this);
+
+        // Add parser for iBeacon.
+        m_BeaconM.getBeaconParsers().add(
+                new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24")
+        );
+
+        m_BeaconM.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        m_BeaconM.unbind(this);
     }
 
     @Override
@@ -48,5 +103,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        m_BeaconM.setRangeNotifier(new MyRangeNotifier());
+        try {
+            m_BeaconM.startRangingBeaconsInRegion(m_Region);
+        }
+        catch (RemoteException e) {
+            // Do nothing.
+        }
     }
 }
